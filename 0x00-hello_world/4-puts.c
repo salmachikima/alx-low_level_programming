e0b64ead7fd0817e5fb05b59540a0c4c4b24682b
salma <salmamarokti888@gmail.com>
2024-06-20 19:38:37 -0700

    betty the pain in

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - this is the main function
+ *
+ * Return:  return 0
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
