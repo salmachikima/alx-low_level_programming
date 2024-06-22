fc3ed522b38a63602004c9004ac3d2bd1799878b
salma <salmamarokti888@gmail.com>
2024-06-21 20:00:37 -0700

    damn alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main fonction
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char s;
+
+	for (s = 'a' ; s <= 'z' ; s++)
+		putchar(s);
+	putchar('\n');
+	return (0);
+}
