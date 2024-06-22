2481c96173b4652791b3d7b3490ce52c933c20e5
salma <salmamarokti888@gmail.com>
2024-06-21 20:49:15 -0700

    helloo

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main fonction
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char s;
+
+	for (s = '0' ; s <= '9' ; s++)
+		putchar(s);
+	putchar('\n');
+	return (0);
+}
