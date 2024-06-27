bc344f92ac9a7fc0ab959a7279009437dea0f92d
salma <salmamarokti888@gmail.com>
2024-06-26 18:52:49 -0700

    beautiful

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,14 +10,14 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-
-	i = 0;
+/*
+*	i = 0;
 
 	while (i < 10)
 	{
-		putchar(i);/*we should write the commande break after*/
-		break;
+		putchar(i);
 	}
+*/
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
