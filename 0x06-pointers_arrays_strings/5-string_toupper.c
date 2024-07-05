1fd6395c9f6f096a9f3fedb373a22bef920085d3
salma <salmamarokti888@gmail.com>
2024-07-04 21:24:19 -0700

    look up!

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *string_toupper - changes all lowrcase to uppercase
+ * @str: string
+ * Return: string
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
