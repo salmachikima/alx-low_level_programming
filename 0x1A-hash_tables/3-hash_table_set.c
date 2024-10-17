1b8b1f0750e5f8642a91ba91f3b8c01a72659d31
salma <salmamarokti888@gmail.com>
2024-10-16 20:57:26 -0700

    set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,19 +1,19 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - Add or update an element in a hash table.
- * @ht: A pointer to the hash table.
- * @key: The key to add - cannot be an empty string.
- * @value: The value associated with key.
+ * hash_table_set - fnctn that Add or update an element in a hash table
+ * @ht: hash table pointer
+ * @key: The key to add
+ * @value: The value to the key
  *
- * Return: Upon failure - 0.
- *         Otherwise - 1.
+ * Return: fail 0
+ *         else 1.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new;
 	char *value_copy;
-	unsigned long int index, i;
+	unsigned long int r, i;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
@@ -22,8 +22,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (value_copy == NULL)
 		return (0);
 
-	index = key_index((const unsigned char *)key, ht->size);
-	for (i = index; ht->array[i]; i++)
+	r = key_index((const unsigned char *)key, ht->size);
+	for (i = r; ht->array[r]; i++)
 	{
 		if (strcmp(ht->array[i]->key, key) == 0)
 		{
@@ -46,8 +46,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 	new->value = value_copy;
-	new->next = ht->array[index];
-	ht->array[index] = new;
+	new->next = ht->array[r];
+	ht->array[r] = new;
 
 	return (1);
 }
