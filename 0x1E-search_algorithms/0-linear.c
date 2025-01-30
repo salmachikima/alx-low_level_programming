365e17b491758eaa28c00cda495d0acb1157eede
salma <salmamarokti888@gmail.com>
2025-01-29 19:20:41 -0800

    all

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - fonction that search for a value
+ * using the linear search algorithm
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: EXIT_SUCCESS
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	int s;
+
+	if (array == NULL)
+		return (-1);
+
+	for (s = 0; s < (int)size; s++)
+	{
+		printf("Value checked array[%u] = [%d]\n", s, array[s]);
+		if (value == array[s])
+			return (s);
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,38 @@
+#include "search_algos.h"
+
+/**
+  * binary_search - searches for a value in a sorted array
+  * of integers using the Binary search algorithm
+  * @array: pointer to the 1st element of the array
+  * @size: number of elemts in the array
+  * @value: value
+  *
+  * Return: value null or not present -1 else the index
+  */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t s;
+	size_t left;
+	size_t right;
+
+	if (array == NULL)
+		return (-1);
+
+	for (left = 0, right = size - 1; right >= left;)
+	{
+		printf("Searching in array: ");
+		for (s = left; s < right; s++)
+			printf("%d, ", array[s]);
+		printf("%d\n", array[s]);
+
+		s = left + (right - left) / 2;
+		if (array[s] == value)
+			return (s);
+		if (array[s] > value)
+			right = s - 1;
+		else
+			left = s + 1;
+	}
+
+	return (-1);
+}
